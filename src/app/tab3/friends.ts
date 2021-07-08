46dc2869adcd9d3fa784e958a749343d4af18fc8
TXT-44 <txt-44@TXT-44.local>
2021-07-07 19:15:32 -0700

    First commit for Ionic challenge

---
+++ src/app/tab3/friends.ts
@@ -0,0 +1,28 @@
+let arr: any[] = [
+    {
+        name: "Cedric",
+        imgSrc: "https://bit.ly/3xrxcF4",
+        phoneNumber: "(504) 252-8072"
+    },
+    {
+        name: "Sarah",
+        imgSrc: "https://bit.ly/2TOcRLJ",
+        phoneNumber: "(323) 508-3599"
+    },
+    {
+        name: "Thomas",
+        imgSrc: "https://bit.ly/3hokO3f",
+        phoneNumber: "(323) 331-1918"
+    },
+    {
+        name: "Million",
+        imgSrc: "https://bit.ly/3wninlF",
+        phoneNumber: "(323) 698-2858"
+    },
+    {
+        name: "Denny",
+        imgSrc: "https://bit.ly/3dTL8zQ",
+        phoneNumber: "(323) 698-5203"
+    },
+]
+export default arr;
